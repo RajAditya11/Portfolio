5270d7799dcbef74d768cbf59d825256491320bd
RajAditya11 <rajadi7185@gmail.com>
2024-11-13 09:49:21 +0530

    Testing mainpage

--- backend.js
+++ backend.js
@@ -4,3 +4,25 @@ document.querySelector(`.back-to-top`).addEventListener(`click`, toTop = () => {
         behavior:`smooth`
     });
 });
+
+
+const section = document.querySelector('#mainScreen');
+const leftContainer = document.querySelector('#leftScreen');
+const rightContainer = document.querySelector('#rightScreen');
+
+section.addEventListener('mousemove' , (e) =>{
+    const rect = section.getBoundingClientRect();
+    const xPos = e.clientX - rect.left;
+
+    const letTotalScreen = `17+00`;
+
+    console.log(rect , xPos , letTotalScreen);
+
+    if( xPos < rect.width / 2 ){
+        leftContainer.style.width = `100%`;
+        rightContainer.style.width = `0`;
+    }else{
+        leftContainer.style.width = `0`;
+        rightContainer.style.width = `100%`;
+    }
+});
